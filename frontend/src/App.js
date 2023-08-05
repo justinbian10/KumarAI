0c3395dda325ba88789dafbf2332acf44fb13be2
Justin Bian <justinbian10@gmail.com>
2023-08-05 17:50:33 -0400

    Add frontend code to the repository

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Query from "./Query";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      </header>
+      <body>
+        <Query />
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Query.js
@@ -0,0 +1,46 @@
+import {useState} from "react";
+
+export default function Query() {
+  const [queryValue, setQueryValue] = useState('');
+  const [output, setOutput] = useState('');
+  const handleTextChange = (e) => {
+    setQueryValue(e.target.value);
+  }
+
+  const handleSubmitClick = () => {
+    console.log('howdy')
+    const requestConfig = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query: queryValue })
+    };
+    // After CORS put back the whole resource url
+    fetch('/query', requestConfig)
+      .then(response => response.text())
+      .then(data => {
+        console.log(data);
+        setOutput(data);
+      });
+  }
+
+  return <div>
+    <QueryInput value={queryValue} onChange={handleTextChange}/>
+    <Submit onSubmitClick={handleSubmitClick}/>
+    <QueryOutput value={output} />
+  </div>
+}
+
+function QueryInput({value, onChange}) {
+  return <label>
+    Please enter your query:
+    <textarea className="big-text" value={value} onChange={event => onChange(event)} />
+  </label>
+}
+
+function Submit( {onSubmitClick}) {
+  return <button className="submit" onClick={onSubmitClick}>Submit</button>
+}
+
+function QueryOutput({ value }) {
+  return <p>{value}</p>
+}
